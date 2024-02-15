d97470f89dd2e5128e560b7056a8e04db6cba756
DevLARLEY <121249322+DevLARLEY@users.noreply.github.com>
2024-02-15 19:04:50 +0100

    forgot this folder ...

---
+++ data/firefox/prefs.js
@@ -0,0 +1,8 @@
+user_pref("media.gmp-gmpopenh264.abi", "x86_64-msvc-x64");
+user_pref("media.gmp-gmpopenh264.hashValue", "b667086ed49579592d435df2b486fe30ba1b62ddd169f19e700cd079239747dd3e20058c285fa9c10a533e34f22b5198ed9b1f92ae560a3067f3e3feacc724f1");
+user_pref("media.gmp-gmpopenh264.version", "2.3.2");
+user_pref("media.gmp-widevinecdm.abi", "x86_64-msvc-x64");
+user_pref("media.gmp-widevinecdm.hashValue", "59521f8c61236641b3299ab460c58c8f5f26fa67e828de853c2cf372f9614d58b9f541aae325b1600ec4f3a47953caacb8122b0dfce7481acfec81045735947d");
+user_pref("media.gmp-widevinecdm.version", "4.10.2710.0");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo on MPDL");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo on MPDL");
